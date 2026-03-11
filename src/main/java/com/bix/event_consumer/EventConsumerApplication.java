fae65bc2814abf067707505349ce7effcc88bf5c
bsteve <bico.steve@betika.com>
2026-03-11 05:03:38 +0300

    added configuration properties on main app and transaction on repo

--- src/main/java/com/bix/event_consumer/EventConsumerApplication.java
+++ src/main/java/com/bix/event_consumer/EventConsumerApplication.java
@@ -2,8 +2,12 @@ package com.bix.event_consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableTransactionManagement
 public class EventConsumerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bix/event_consumer/repositories/EventRepository.java
+++ src/main/java/com/bix/event_consumer/repositories/EventRepository.java
@@ -1,6 +1,7 @@
 package com.bix.event_consumer.repositories;
 
 import com.bix.event_consumer.models.Event;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -12,9 +13,11 @@ import org.springframework.stereotype.Repository;
 public class EventRepository {
     private final JdbcTemplate jdbcTemplate;
 
+    @Transactional
     public void updateEvent(Event event){
         log.info("ConsumerRepo::Attempt to insert event {}",event.getEventId());
         log.info("ConsumerRepo::event {}",event);
+        this.jdbcTemplate.update("");
 
     }
 }
